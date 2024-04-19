1625eaa879a5e83614258d3eb01e1524f8a8a40c
Blane Richoux <brx.cybr@gmail.com>
2024-04-19 11:43:23 -0400

    Minor fixes to Get-BAM

--- forensics/Get-BAM.ps1
+++ forensics/Get-BAM.ps1
@@ -21,12 +21,19 @@ Last Updated: 3/10/2020
 #>
 
 $regPath = "Registry::HKLM\SYSTEM\CurrentControlSet\Services\bam\State\UserSettings\"
-$regKeyNames = Get-ChildItem -Name
+$regKeyNames = Get-ChildItem -Name $regPath
 $data = @()
 foreach ($SID in $regKeyNames){
 	$keyPath = $regPath + $SID
     $keyProperty = Get-ItemProperty -Path $keyPath
-    $Entries = $keyProperty | get-member -MemberType NoteProperty | ? {$_.Name -notmatch "\bPS\w+\b" -and $_.Name -ne "Version" -and $_.Name -ne "SequenceNumber" } | select -ExpandProperty name
+    $Entries = $keyProperty | 
+        Get-Member -MemberType NoteProperty | 
+        Where-Object {
+            $_.Name -notmatch "\bPS\w+\b" `
+            -and $_.Name -ne "Version" `
+            -and $_.Name -ne "SequenceNumber" 
+        } | 
+        Select-Object -ExpandProperty Name
     foreach( $key in $Entries){ 
         $value = $keyProperty.$key
         $hex  = [System.BitConverter]::ToString($value) -split '-'
